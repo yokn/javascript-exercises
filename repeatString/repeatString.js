6c8ef8795dbe6231de13d3e6b6ecb8bbfa255929
yokn <discolouredattic@gmail.com>
2020-06-16 20:11:47 +0300

    all tests passing

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatCount) {
+    if (string!=""&&repeatCount!=0){
+        if(repeatCount<0){
+            return "ERROR"
+        }
+        let newString = string
+        for(i=1;i<repeatCount;i++){
+            
+            newString = newString + string 
+        }
+        return newString
+    }
+    else{
+        return ""
+    }
+    
 }
 
 module.exports = repeatString
