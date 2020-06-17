77fd8f967290fe4a68af17cc5f90e8b95b0cf70d
yokn <discolouredattic@gmail.com>
2020-06-17 20:21:04 +0300

    passing first test

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+        if (year % 100) {
+                if (year % 400) {
+                        return true;
+                }
+                return false;
+        }
+        if (year % 4) {
+                return true;
+        }
+        return false;
+};
 
-}
-
-module.exports = leapYears
+module.exports = leapYears;
