7af3930b50347fcafc821ed0865cc0b1247bd080
yokn <discolouredattic@gmail.com>
2020-06-17 19:45:04 +0300

    passing all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-bitwise */
+
 const sumAll = function(x, y) {
         let total = 0;
-        for (; x <= y; x++) {
-                total += x;
+        const biggerNumber = Math.max(x, y);
+        const lowerNumber = Math.min(x, y);
+        // eslint-disable-next-line prettier/prettier
+        if (!(x & y > 0)) {
+                return 'ERROR';
+        }
+
+        for (let i = lowerNumber; i <= biggerNumber; i++) {
+                total += i;
         }
         return total;
 };
