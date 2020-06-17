cdf56f316dca0711dbdea4b17f98f70b6a70e835
yokn <discolouredattic@gmail.com>
2020-06-17 19:29:05 +0300

    passing first test

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+        let total = 0;
+        for (; x <= y; x++) {
+                total += x;
+        }
+        return total;
+};
 
-}
-
-module.exports = sumAll
+module.exports = sumAll;
