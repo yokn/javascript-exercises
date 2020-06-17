553a86611297373e2f40d21e1f1b7c3b45fe7a83
yokn <discolouredattic@gmail.com>
2020-06-17 17:32:48 +0300

    major rewrite all tests passing

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,39 +1,11 @@
 /* eslint-disable no-console */
 
-const removeFromArray = function(inputArray, ...inputRemove) {
-        const array = Array.from(inputArray);
-        const toRemove = inputRemove;
-        let j = 0;
-
-        // console.log(array);
-        // console.log(toRemove);
-
-        // function remover(element) {
-        //        return element != inputRemove;
-        // }
-        for (let i = 0; i < array.length; i++) {
-                if (array[i] === toRemove[j]) {
-                        const newArray = array.splice(i, 1);
-                        i = 0;
-                        j++;
-                        // console.log(newArray);
-                        // console.log(array);
-                }
-        }
-        /*  this leaves undefined values inside the slots instead of removing them
-        let result = replace(array,toRemove)
-        function replace(array,toRemove){
-            let newArray
-            newArray=array.join(" ")
-            console.log(newArray)
-            newArray=newArray.replace(toRemove,"")
-            return newArray
-        }
-        */
-
-        // const filteredArray = array.filter(remover);
-        // return filteredArray;
-        return array;
+// had to cheat after my previous solution didn't work for the last 4 tests
+const removeFromArray = function(...input) {
+        let array = [];
+        array = input[0];
+        const newArray = array.filter(element => !input.includes(element));
+        return newArray;
 };
 
 module.exports = removeFromArray;
