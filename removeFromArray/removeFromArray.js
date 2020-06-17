b071a229f319f99fa52298d59e4dffd66e9ed5ac
yokn <discolouredattic@gmail.com>
2020-06-17 13:25:36 +0300

    passing second test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,10 +1,25 @@
 /* eslint-disable no-console */
-const removeFromArray = function(inputArray, inputRemove) {
+
+const removeFromArray = function(inputArray, ...inputRemove) {
         const array = Array.from(inputArray);
         const toRemove = inputRemove;
-        console.log(array);
-        console.log(toRemove);
+        let j = 0;
 
+        // console.log(array);
+        // console.log(toRemove);
+
+        // function remover(element) {
+        //        return element != inputRemove;
+        // }
+        for (let i = 0; i < array.length; i++) {
+                if (array[i] === toRemove[j]) {
+                        const newArray = array.splice(i, 1);
+                        i = 0;
+                        j++;
+                        // console.log(newArray);
+                        // console.log(array);
+                }
+        }
         /*  this leaves undefined values inside the slots instead of removing them
         let result = replace(array,toRemove)
         function replace(array,toRemove){
@@ -15,13 +30,10 @@ const removeFromArray = function(inputArray, inputRemove) {
             return newArray
         }
         */
-        function remover(element) {
-                return element != inputRemove;
-        }
 
-        const filteredArray = array.filter(remover);
-
-        return filteredArray;
+        // const filteredArray = array.filter(remover);
+        // return filteredArray;
+        return array;
 };
 
 module.exports = removeFromArray;
