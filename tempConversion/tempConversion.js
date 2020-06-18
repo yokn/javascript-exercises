ea232742082fbda8654105b062dac19e39092166
yokn <discolouredattic@gmail.com>
2020-06-18 10:30:48 +0300

    all tests passing

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const ftoc = function(inputF) {
-        return Math.floor((5 / 9) * (inputF - 32));
+        return Math.round((5 / 9) * (inputF - 32) * 10) / 10;
 };
 
 const ctof = function(inputC) {
-        return Math.floor((9 / 5) * inputC + 32);
+        return Math.round(((9 / 5) * inputC + 32) * 10) / 10;
 };
 
 module.exports = {
