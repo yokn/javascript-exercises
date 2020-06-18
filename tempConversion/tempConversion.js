b453dab94e8ff70069b0ef3204bf6ca2ff54979b
yokn <discolouredattic@gmail.com>
2020-06-18 10:25:00 +0300

    passing first test

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-const ftoc = function() {
+const ftoc = function(inputF) {
+        return Math.floor((5 / 9) * (inputF - 32));
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function(inputC) {
+        return Math.floor((9 / 5) * inputC + 32);
+};
 
 module.exports = {
-  ftoc,
-  ctof
-}
+        ftoc,
+        ctof,
+};
