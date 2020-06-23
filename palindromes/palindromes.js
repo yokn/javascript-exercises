73b7ed7801929ac093a0ee5a6f8cad249118f671
yokn <discolouredattic@gmail.com>
2020-06-23 20:44:32 +0300

    passing first test

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,18 +1,38 @@
 const palindromes = function(input) {
-        let split;
-        if (input.length % 2 === 0) {
-                split = input.split('', input.length / 2);
-                console.log(split);
-                if (split === input.reverse()) {
+        const string = input.toString();
+        let substring;
+        let reversed;
+        let reversedSubstring;
+        if (string.length % 2 === 0) {
+                substring = string.substring(0, input.length / 2);
+                console.log(substring);
+                reversed = string
+                        .split('')
+                        .reverse()
+                        .join('');
+                console.log(reversed);
+                reversedSubstring = reversed.substring(0, input.length / 2);
+                console.log(`${reversedSubstring}1`);
+                if (substring === reversedSubstring) {
                         return true;
                 }
-        } else {
-                split = input.split('', input.length / 2 + 1); // not sure about this
-                console.log(split);
-                if (split === input.reverse()) {
-                        return true;
-                }
+
+                return false;
         }
+
+        substring = input.substring(0, input.length / 2 + 1); // not sure about this
+        console.log(substring);
+        reversed = string
+                .split('')
+                .reverse()
+                .join('');
+        console.log(reversed);
+        reversedSubstring = reversed.substring(0, input.length / 2 + 1);
+        console.log(reversedSubstring);
+        if (substring === reversedSubstring) {
+                return true;
+        }
+        return false;
 };
 
 module.exports = palindromes;
