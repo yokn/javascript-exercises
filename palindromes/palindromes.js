5ffd763ff41f005631ff2efb60b197960ae828af
yokn <discolouredattic@gmail.com>
2020-06-23 16:49:31 +0300

    initial commit

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function() {
+const palindromes = function(input) {
+        let split;
+        if (input.length % 2 === 0) {
+                split = input.split('', input.length / 2);
+                console.log(split);
+                if (split === input.reverse()) {
+                        return true;
+                }
+        } else {
+                split = input.split('', input.length / 2 + 1); // not sure about this
+                console.log(split);
+                if (split === input.reverse()) {
+                        return true;
+                }
+        }
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
