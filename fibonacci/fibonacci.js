a3322d54e52c45fd61bcab8d47992a77c5fb5247
yokn <discolouredattic@gmail.com>
2020-06-25 11:14:20 +0300

    passing first test

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+        let n = 0;
+        let nPrev = 1;
+        let nOld = 1;
+        for (let i = 0; i < input - 2; i++) {
+                n = nPrev + nOld;
+                nOld = nPrev;
+                nPrev = n;
+        }
+        return n;
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
