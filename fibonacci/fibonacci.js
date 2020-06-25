48fc17e873df447beda58805183593680dcccaea
yokn <discolouredattic@gmail.com>
2020-06-25 11:17:31 +0300

    doesnt accept negatives

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,13 +1,16 @@
 const fibonacci = function(input) {
-        let n = 0;
-        let nPrev = 1;
-        let nOld = 1;
-        for (let i = 0; i < input - 2; i++) {
-                n = nPrev + nOld;
-                nOld = nPrev;
-                nPrev = n;
+        if (input == Math.abs(input)) {
+                let n = 0;
+                let nPrev = 1;
+                let nOld = 1;
+                for (let i = 0; i < input - 2; i++) {
+                        n = nPrev + nOld;
+                        nOld = nPrev;
+                        nPrev = n;
+                }
+                return n;
         }
-        return n;
+        return 'OOPS';
 };
 
 module.exports = fibonacci;
