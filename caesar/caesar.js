8ca8649a25974e3c8795a5048ad12bd5f1c6dc2b
yokn <discolouredattic@gmail.com>
2020-06-25 10:29:16 +0300

    refactoring

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable no-console */
 const caesar = function(string, shiftAmount) {
         const alphabetUpperCase = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
         const alphabetLowerCase = Array.from('abcdefghijklmnopqrstuvwxyz');
@@ -18,18 +16,13 @@ const caesar = function(string, shiftAmount) {
         function shifter(char, upperCase) {
                 let j = 0;
                 let charPos = 0;
-                let over26 = 0;
-                if (upperCase === true) {
-                        while (charPos === 0) {
-                                if (j + shiftAmount >= 26) {
-                                        over26++;
-                                        console.log(`over26 count: ${over26}`);
-                                        shiftAmount -= 26;
-                                }
+
+                while (charPos === 0) {
+                        if (j + shiftAmount >= 26) {
+                                shiftAmount -= 26;
+                        }
+                        if (upperCase === true) {
                                 if (char === alphabetUpperCase[j]) {
-                                        if (over26 > 0) {
-                                                console.log('Triggered me!!');
-                                        }
                                         charPos += j + shiftAmount;
                                         console.log(`charPos is ${charPos}`);
                                         if (charPos >= 0) {
@@ -38,21 +31,8 @@ const caesar = function(string, shiftAmount) {
 
                                         return alphabetUpperCaseReverse[Math.abs(charPos) - 1];
                                 }
-
-                                j++;
-                                console.log(`j is: ${j}`);
-                        }
-                }
-                while (charPos === 0) {
-                        if (j + shiftAmount >= 26) {
-                                over26++;
-                                console.log(`over26 count: ${over26}`);
-                                shiftAmount -= 26;
                         }
                         if (char === alphabetLowerCase[j]) {
-                                if (over26 > 0) {
-                                        console.log('Triggered me!!');
-                                }
                                 charPos += j + shiftAmount;
                                 console.log(`charPos is ${charPos}`);
                                 if (charPos >= 0) {
@@ -67,28 +47,15 @@ const caesar = function(string, shiftAmount) {
                 }
         }
 
-        function main() {
-                for (let i = 0; i < string.length; i++) {
-                        if (/[A-Za-z]/.test(splitString[i])) {
-                                splitString[i] = shifter(splitString[i], checkUpperCase(splitString[i]));
-                                // console.log(typeof splitString);
-                                // Apparently splitString is not a "real" array anymore so I have to do this
-                                newArray = Array.from(splitString);
-                                newArray = newArray.join('', '');
-                                console.log(`output = ${newArray}`);
-                        }
+        for (let i = 0; i < string.length; i++) {
+                if (/[A-Za-z]/.test(splitString[i])) {
+                        splitString[i] = shifter(splitString[i], checkUpperCase(splitString[i]));
+                        newArray = Array.from(splitString);
+                        newArray = newArray.join('', '');
+                        console.log(`output = ${newArray}`);
                 }
-                return newArray;
         }
-        return main();
+        return newArray;
 };
-
-/*
-0 Take the string and the shift amount as input
-1 
-2 Shift the string 
-3 
-9 Return the string
-*/
 
 module.exports = caesar;
